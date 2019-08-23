dd413abfe6f98190cffb36f7ea06ad4db74f941a
ajhamwood <ajh@tuta.io>
2019-08-24 01:02:17 +1000

    Progress on photo image collection page

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sockets import Sockets
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
+
 app = Flask(__name__)
+app.debug = True
+
+sockets = Sockets(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@sockets.route('/ws')
+def trans(ws):
+    while not ws.closed:
+        gevent.sleep(0.1)
+        message = ws.receive()
+        if message:
+            dir(message)
+
+
+if __name__ == '__main__':
+    pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler) \
+          .serve_forever()
