18b25a2ee907b42e65eeb5a9896aa8369c118f1e
ajhamwood <ajh@tuta.io>
2019-08-25 08:34:33 +1000

    orientation data in AR machine OK

--- static/js/app.js
+++ static/js/app.js
@@ -21,7 +21,10 @@ $.targets({
     app.emit('init');
   },
 
-  resize () { app.emit('resize') },
+  resize () {
+    app.emit('resize');
+    ar.emit('resize')
+  },
 
   deviceorientation (e) {
     var alpha;
@@ -31,9 +34,12 @@ $.targets({
     else {
       alpha = e.alpha;
       // Assume Android stock
-      if (!window.chrome) alpha = alpha-270
+      if (!window.chrome) alpha = alpha - 270
     }
-    // $('#debug')[0].textContent = alpha
+    let { beta, gamma } = e;
+    ar.state().rotationObservers.forEach(ofn => ofn({alpha, beta, gamma}))
+
+    // app.emit('debug', alpha)
   },
 
   app: {
@@ -56,7 +62,9 @@ $.targets({
       return fetch('/', {method: 'POST', body})
         .then(res => res.json()).then(data => {
           if ('ok' in data) return app.emitAsync('startCamera')
+            .then(() => ar.emitAsync('init'))
             .catch(e => {
+              console.log(e);
               this.hasCamera = false;
               $('#displayToggle')[0].classList.add('hide');
               $('.feedback').forEach(el => el.classList.toggle('active'))
@@ -64,7 +72,10 @@ $.targets({
             .then(() => app.emitAsync('showDest'));
           else throw new Error('Could not post data')
         })
-        .catch(console.log)
+        .catch(e => {
+          app.emit('debug', e.message)
+          console.log(e)
+        })
     },
 
     setMarker (lat, lng) {
@@ -78,7 +89,6 @@ $.targets({
       let {latitude, longitude} = this.destination,
           vmin = Math.min(document.body.clientHeight, document.body.clientWidth) / 2;
       let dist = getDistanceFromLatLon(latitude, longitude, curLat, curLng);
-      // $('#debug')[0].textContent = dist
       this.map.setOptions({zoom: getZoomLevel(latitude, dist, vmin)});
     },
 
@@ -101,7 +111,9 @@ $.targets({
 
     heartbeat (on) {
       this.heartbeat = on ?
-        this.iv = setTimeout(() => {}, 1000) :
+        this.iv = setTimeout(() => $.pipe('heartbeat',
+          () => app.emitAsync('sendData'),
+          () => app.emitAsync('heartbeat', this.heartbeat)), 1000) :
         clearTimeout(this.iv)
     },
 
@@ -109,6 +121,12 @@ $.targets({
       let body = new FormData(), update = { latitude, longitude };
       body.append('timestamp', Date.now());
       body.append('geo', JSON.stringify({ update }));
+      if (this.hasCamera && $('.feedback.active')[0].id === 'ar' && false) {
+        let { width, height } = this.photoCanvas;
+        this.photoContext.drawImage(this.video, 0, 0, width, height);
+        let { data } = this.photoContext.getImageData(0, 0, width, height);
+        body.append('imgbuf', new Blob([data]), 'imgbuf');
+      }
       return fetch('/', {method: 'POST', body})
     },
 
@@ -119,8 +137,8 @@ $.targets({
       let { clientWidth, clientHeight } = document.body;
       this.photoCanvas.width = clientWidth / 4;
       this.photoCanvas.height = clientHeight / 4;
-      $('canvas')[0].width = $('video')[0].width = clientWidth;
-      $('canvas')[0].height = $('video')[0].height = clientHeight;
+      $('canvas')[0].width = this.video.width = clientWidth;
+      $('canvas')[0].height = this.video.height = clientHeight
     },
 
 
@@ -130,15 +148,9 @@ $.targets({
       let { latitude, longitude } = pos.coords;
       this.geo = { latitude, longitude };
       let gGeo = new google.maps.LatLng(...Object.values(this.geo));
-      this.map.setCenter(gGeo)
+      this.map.setCenter(gGeo);
       app.emit('setZoom', latitude, longitude);
       app.emit('sendData', latitude, longitude)
-    },
-
-
-    // Orientation
-    updateOrient () {
-      this.orient
     },
 
 
@@ -146,21 +158,25 @@ $.targets({
     startCamera () {
       if (navigator.mediaDevices.getUserMedia) {
         return navigator.mediaDevices.getUserMedia({
-          video: { width: 4800, height: 6400, facingMode: { exact: 'environment' } }
+          video: { width: 4800, height: 6400/*, facingMode: { exact: 'environment' }*/ }
         }).then(s => {
           this.video.srcObject = s;
           this.video.onloadedmetadata = () => this.video.play();
-        }).catch(e => { throw e })
+        })
       } else throw new Error('Can\'t connect camera')
-    }
+    },
+
+
+    // Debug
+    debug (...strings) { $('#debug')[0].textContent = strings.join(' ') }
 
   }
 });
 
 $.queries({
   '#map': {
-    touchstart () { $.pipe('titleblur', () => new Promise(r => setTimeout(() => r($('#title')[0].classList.add('blur')), 500))) },
-    touchend () { $.pipe('titleblur', () => new Promise(r => setTimeout(() => r($('#title')[0].classList.remove('blur')), 500))) }
+    'mousedown touchstart' () { $.pipe('titleblur', () => new Promise(r => setTimeout(() => r($('.padded-multiline')[0].classList.add('blur')), 500))) },
+    'mouseup touchend' () { $.pipe('titleblur', () => new Promise(r => setTimeout(() => r($('.padded-multiline')[0].classList.remove('blur')), 500))) }
   },
   '#title': {
     click () {
@@ -204,3 +220,104 @@ function getDistanceFromLatLon(lat1,lon1,lat2,lon2) {
   var d = R * c;
   return d;
 }
+
+
+// AR
+var ar = new $.Machine({
+  arCanvas: $('#arGUI')[0],
+  renderer: null,
+  camera: null,
+  scene: null,
+  geom: null,
+  material: null,
+  object: null,
+
+  rotationObservers: [],
+  obsFlag: false,
+  pause: null
+});
+
+let startTime = Date.now()
+
+$.targets({
+  ar: { // TODO: pause while in map mode
+
+    init () {
+      let width = window.innerWidth,
+          height = window.innerHeight;
+
+      this.scene = new THREE.Scene();
+      this.geom = new THREE.BoxGeometry(1, 1, 1);
+      this.renderer = new THREE.WebGLRenderer({canvas: this.arCanvas, antialias: true, alpha: true});
+      this.camera = new THREE.PerspectiveCamera(75, width / height, .01, 1000);
+      this.camera.position.z = Math.hypot(2, 2, 2);
+      ar.emit('resize');
+      this.scene.add(this.camera);
+      this.renderer.domElement.id = "renderer";
+
+      let pointLight = new THREE.PointLight(0xffffff);
+      //Object.assign(pointLight.position, {x:6,y:5,z:17});
+      pointLight.position.x = 6
+      pointLight.position.y = 5
+      pointLight.position.z = 17
+      this.scene.add(pointLight);
+
+      ar.emit('updateMaterial');
+      ar.emit('createObject')
+    },
+
+    resize () {
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix()
+    },
+
+    updateMaterial () {
+      this.material = new THREE.MeshNormalMaterial({
+        transparent: true, opacity: .5
+      });
+      this.material.side = THREE.DoubleSide
+    },
+
+    createObject () {
+      let { geom, material, scene, object } = this,
+          objnew = new THREE.Mesh(geom, material);
+      objnew.rotation.set(0, 0, 0);
+      if (object) {
+        objnew.rotation = object.rotation;
+        objnew.scale = object.scale;
+        scene.remove(object)
+      }
+      scene.add(objnew);
+      this.object = objnew;
+      ar.emit('unpause')
+    },
+
+    animate ({alpha, beta, gamma}, fromObs) {
+
+
+			this.object.rotation.x += 0.02;
+			this.object.rotation.y += 0.0225;
+			this.object.rotation.z += 0.0175;
+				// make the cube bounce
+			var dtime	= Date.now() - startTime;
+			this.object.scale.x	= 1 + 0.3*Math.sin(dtime/300);
+			this.object.scale.y	= 1 + 0.3*Math.sin(dtime/300);
+			this.object.scale.z	= 1 + 0.3*Math.sin(dtime/300);
+
+
+      this.renderer.render(this.scene, this.camera);
+      if (!this.pause && !this.obsFlag) requestAnimationFrame(() => ar.emit('animate', {alpha, beta, gamma}, false));
+      this.obsFlag = fromObs
+    },
+
+    pause () { this.pause = true },
+    unpause () {
+      this.pause = false;
+      this.rotationObservers.push(({alpha, beta, gamma}) =>
+        ar.emit('animate', {alpha, beta, gamma}, true))
+      ar.emit('animate', {}, false)
+    }
+
+  }
+})
