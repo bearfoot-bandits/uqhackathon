159c8b4d13625fd0321681ad4e677b18a24becc6
apache10 <gagu12ec@gmail.com>
2019-08-24 14:52:34 +1000

    app has been updated with database connection

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import cv2
+from matplotlib import pyplot as plt
+from bson.json_util import dumps
 
 app = Flask(__name__)
 app.debug = True
+client = MongoClient('localhost', 27017)
+db = client.sample
+collection = db.location
+
+
+@app.route('/testDb', methods=['GET'])
+def testDb():
+    # x = db.users.insert({
+    #     "id":
+    #     1001,
+    #     "user_name":
+    #     "rahul",
+    #     "name": [{
+    #         "first_name": "Rahul"
+    #     }, {
+    #         "middle_name": ""
+    #     }, {
+    #         "last_name": "Kumar"
+    #     }],
+    #     "email":
+    #     "rahul@tecadmin.net",
+    #     "location":
+    #     "dont know"
+    # })
+    y = dumps((db.users.find({'location': 'dont know'})))
+    print("anything")
+    return (y)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/reference', methods=['POST'])
 def trans():
@@ -15,10 +48,41 @@ def trans():
     timestamp = request.form['timestamp']
     geo = request.form['geo']
     orient = request.form['orient']
+    #to store in database
+    data = {}
+    data['imgbuf'] = imgbuf
+    data['timestamp'] = timestamp
+    data['orient'] = orient
+    #data['lat'] = geo.lat
+    #data['lng'] = geo.lng
+
+    #print(dumps((db.users.find({}))))
+    db.users.insert(data)
 
     print(imgbuf)
-    return jsonify(ok='ok') # TODO: return {} if the data is incorrect or if there is an error
+    return jsonify(
+        ok='ok'
+    )  # TODO: return {} if the data is incorrect or if there is an error
 
+
+@app.route('/findLoc', methods=['POST'])
+def findLoc():
+    print(request.files)
+    imgbuf = request.files.get('imgbuf')
+    timestamp = request.form['timestamp']
+    geo = request.form['geo']
+    orient = request.form['orient']
+    #to store in database
+    data = {}
+    data['imgbuf'] = imgbuf
+    data['timestamp'] = timestamp
+    data['orient'] = orient
+    #data['lat'] = geo.lat
+    #data['lng'] = geo.lng
+
+    return jsonify(
+        ok='ok'
+    )  # TODO: return {} if the data is incorrect or if there is an error
 
 
 if __name__ == '__main__':
